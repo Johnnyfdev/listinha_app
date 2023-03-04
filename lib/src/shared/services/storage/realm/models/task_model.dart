03a51bcf19515899566b18c8fd41ac775f845be9
JohnnyFdev <johnnyfreirejh@hotmail.com>
2023-03-03 23:38:59 -0300

    created task and taskboard model

--- lib/src/shared/services/storage/realm/models/task_model.dart
+++ lib/src/shared/services/storage/realm/models/task_model.dart
@@ -1,9 +1,20 @@
 import 'package:realm/realm.dart';
 
+part 'task_model.g.dart';
+
 @RealmModel()
 class _TaskModel {
   @PrimaryKey()
   late Uuid id;
   late String description;
   bool isCompleted = false;
+}
+
+@RealmModel()
+class _TaskBoardModel {
+  @PrimaryKey()
+  late Uuid id;
+  late String title;
+  late List<_TaskModel> tasks;
+  bool isEnable = true;
 }
--- lib/src/shared/services/storage/realm/realm_config.dart
+++ lib/src/shared/services/storage/realm/realm_config.dart
@@ -1,14 +1,18 @@
 import 'package:realm/realm.dart';
 
 import 'models/settings_model.dart';
+import 'models/task_model.dart';
 
 LocalConfiguration config = Configuration.local(
   [
     SettingsModel.schema,
+    TaskModel.schema,
+    TaskBoardModel.schema,
   ],
   initialDataCallback: (realm) {
     realm.add(
       SettingsModel('system'),
     );
   },
+  schemaVersion: 2,
 );
